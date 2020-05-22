14fd58d0eb0e7d02c2155f3a3e652bd6c23447bd
LNPappas <LPappas@gmail.com>
2020-05-22 16:21:25 -0700

    HelloWorld.java

--- Java100/examples/MyFirstProgram.java
+++ Java100/examples/MyFirstProgram.java
@@ -1,5 +1,14 @@
 package examples;
 
+// static: the method exists before the class is instantiated
+//     The method belongs to the class, not the object.
+
+// void: return type. Nothing is returned. 
+
+// main method: never returns anything
+//      Once executed program is done.
+
+
 public class MyFirstProgram {
 
     public static void message(String myString, int myInt) {
